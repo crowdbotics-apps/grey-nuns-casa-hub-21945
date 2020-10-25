aa6f8c66134dd37119a30e0d58305b4a08d44a15
Crowdbotics <team@crowdbotics.com>
2020-10-25 18:44:38 +0000

    add package blueprint-react-native-addItem-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Add-Item161460', human_name: 'Add-item', access_route: 'Add-Item161460'},
 { name: 'Maps161456', human_name: 'Maps', access_route: 'Maps161456', icon: 'map'},
 { name: 'UserProfile161452', human_name: 'User Profile', access_route: 'UserProfile161452'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Add-Item161460Navigator from '../features/Add-Item161460/navigator';
 import Maps161456Navigator from '../features/Maps161456/navigator';
 import UserProfile161452Navigator from '../features/UserProfile161452/navigator';
 
@@ -15,6 +16,7 @@ import UserProfile161452Navigator from '../features/UserProfile161452/navigator'
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Add-Item161460: { screen: Add-Item161460Navigator },
 Maps161456: { screen: Maps161456Navigator },
 UserProfile161452: { screen: UserProfile161452Navigator },
 
