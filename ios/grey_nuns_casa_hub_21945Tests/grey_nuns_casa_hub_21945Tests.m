7529ee730eca55c5d21be3f5af16d9d89f32b3a6
Crowdbotics <team@crowdbotics.com>
2020-10-25 16:45:21 +0000

    Initial commit.

---
+++ ios/grey_nuns_casa_hub_21945Tests/grey_nuns_casa_hub_21945Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface grey_nuns_casa_hub_21945 : XCTestCase
+
+@end
+
+@implementation grey_nuns_casa_hub_21945
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
