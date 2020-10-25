5a2d8fb3f6f1fbc008d78cc33b820c2ab68b8ae0
Crowdbotics <team@crowdbotics.com>
2020-10-25 18:43:30 +0000

    Update models

--- backend/grey_nuns_casa_hub_21945/wsgi.py
+++ backend/grey_nuns_casa_hub_21945/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grey_nuns_casa_hub_21945.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grey_nuns_casa_hub_21945.settings")
 
 application = get_wsgi_application()
